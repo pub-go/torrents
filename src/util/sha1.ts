49dc51ea27051c49e5af113c867fd07189c7dea4
youthlin <youthlinchen@outlook.com>
2023-05-17 16:42:29 +0800

    worker

--- src/util/sha1.ts
+++ src/util/sha1.ts
@@ -1,13 +1,10 @@
 import type { UploadRawFile } from "element-plus"
+import type { Req, Resp } from "./worker"
 
-async function SHA1(data: string) {
-    return sha1(new TextEncoder().encode(data))
-}
-
-async function sha1(data: BufferSource): Promise<ArrayBuffer> {
+async function sha1(data: BufferSource): Promise<Uint8Array> {
     // https://developer.mozilla.org/zh-CN/docs/Web/API/SubtleCrypto/digest
     const buf = await crypto.subtle.digest('SHA-1', data)
-    return buf
+    return new Uint8Array(buf)
 }
 
 function readFiles(files: UploadRawFile[], totalSize: number, pieceSize: number) {
@@ -42,38 +39,33 @@ function readFiles(files: UploadRawFile[], totalSize: number, pieceSize: number)
                         sum.set(new Uint8Array(part2), part.byteLength)
                         part = sum
                     }
-                    return { done: false, value: part }
+                    return { done: false, value: new Uint8Array(part) }
                 }
             }
         }
     }
 }
 
-type onProgress = (current: number, total: number, version: string) => boolean
-
+// 算完后点击上一步再点击下一步 如果文件和分片大小没变 就复用 sha1 结果
 const cache = new Map<string, Uint8Array>()
+// 进度回调。如果返回 false 则终止计算
+type onProgress = (current: number, total: number, version: string) => boolean
 
 async function genFilesSha1(files: UploadRawFile[], pieceSize: number, version: string, onProgress: onProgress) {
     const totalSize = files.reduce((sum, f) => sum + f.size, 0)
+
+    // cache first
     const key = files.map(f => f.uid).join('|') + pieceSize
     const cached = cache.get(key)
     if (cached) {
         onProgress(totalSize, totalSize, version)
         return cached
     }
-    let readBytes = 0
-    const hashArray: Uint8Array[] = []
-    for await (let piece of readFiles(files, totalSize, pieceSize)) {
-        if (piece) {
-            readBytes += piece.byteLength
-            if (!onProgress(readBytes, totalSize, version)) {
-                throw new Error('Canceled')
-            }
-            const hash = await sha1(piece)
-            hashArray.push(new Uint8Array(hash))
-        }
-    }
 
+    // 对每个分片计算 sha1 值
+    const hashArray: Uint8Array[] = await genPiecesSha1(files, pieceSize, totalSize, version, onProgress)
+
+    // 结果组合 将分片结果连起来返回
     const len = hashArray.reduce((sum, i) => sum + i.byteLength, 0)
     const result = new Uint8Array(len)
     let i = 0
@@ -81,8 +73,109 @@ async function genFilesSha1(files: UploadRawFile[], pieceSize: number, version: 
         result.set(h, i)
         i += h.byteLength
     }
+
+    // 写回缓存
     cache.set(key, result)
     return result
 }
 
-export { SHA1, sha1, genFilesSha1 }
+async function genPiecesSha1(files: UploadRawFile[], pieceSize: number, totalSize: number, version: string, onProgress: onProgress) {
+    const hashArray: Uint8Array[] = []
+    let index = 0// 分片位置
+    let readBytes = 0// 已读取字节数 用于更新进度
+    let doneBytes = 0
+    await new Promise<void>(async (resolve) => {
+        for await (let piece of readFiles(files, totalSize, pieceSize)) {
+            if (piece) {
+                readBytes += piece.byteLength
+                if (!onProgress(readBytes, totalSize, version)) {
+                    throw new Error('Canceled')// 如果界面上重置了任务允许终端计算
+                }
+                await hash(index++, piece, (index, pieceLength, result) => {
+                    hashArray[index] = result // 分片结果保存
+                    doneBytes += pieceLength // 已计算完成的字节数
+                    if (doneBytes === totalSize) { resolve() }
+                })// await 返回才继续读取下一分片
+            }
+        }
+    })// 等待所有分片计算完成
+
+    return hashArray
+}
+
+type HashCallback = (pieceIndex: number, pieceLength: number, pieceHash: Uint8Array) => void
+
+const workers = (() => {
+    const result: Worker[] = []
+    try {
+        const maxWorkerCount = Math.min(navigator.hardwareConcurrency || 1, 8);
+        for (let index = 0; index < maxWorkerCount; index++) {
+            result.push(new Worker(new URL('./worker', import.meta.url)))
+        }
+    } catch (e) { console.log('init worker error', e) }
+    console.log('workes length', result.length, result)
+    return result
+})()
+
+// 计算 sha1 值。返回一个 Promise<void> 当其完成后才能继续读取下一分片。
+// 当有多个 worker 时, 任务提交给可用的 worker 后即可继续读取下一分片。
+async function hash(pieceIndex: number, pieceData: Uint8Array, callback: HashCallback) {
+    return new Promise<void>((resolve, reject) => {
+        if (workers.length > 0) {
+            doHashWithWorker(pieceIndex, pieceData, callback, resolve)
+        } else {// 非 worker 模式
+            sha1(pieceData).then((result: Uint8Array) => {
+                callback(pieceIndex, pieceData.byteLength, result)
+                resolve() // 计算完后再读取接下来的数据
+            })
+        }
+    })
+}
+
+// 正在运行的 workers 的下标
+const workingIndex = new Set<number>()
+// 等待队列
+const waitingTask: { req: Req, callback: HashCallback, resolve: () => void }[] = []
+
+function doHashWithWorker(pieceIndex: number, pieceData: Uint8Array, callback: HashCallback, resolve: () => void) {
+    let selectedWorker: Worker | null = null
+    let selectIndex: number = -1
+    for (let i = 0; i < workers.length; i++) {
+        if (!workingIndex.has(i)) { // 该 worker 可用
+            workingIndex.add(i) // 置为工作中
+            selectIndex = i
+            selectedWorker = workers[i]
+            break
+        }
+    }
+
+    const req: Req = {
+        workerIndex: selectIndex,
+        pieceIndex: pieceIndex,
+        pieceData: pieceData,
+    }
+    // console.log('req=', req.workerIndex, req.pieceIndex)
+    const start = new Date().getTime()
+    if (selectedWorker != null) { // 发送消息给 worker 处理
+        resolve()// 有 worker 可以处理，就可以继续读取接下来的数据
+        selectedWorker.postMessage(req)
+        selectedWorker.onmessage = (e) => {
+            const resp = e.data as Resp // 收到结果
+            const cost = new Date().getTime() - start
+            // console.log('cost', duration(cost), 'resp=', resp)
+            callback(resp.pieceIndex, resp.pieceLengh, resp.pieceHash)
+            workingIndex.delete(resp.workerIndex)// 将该 worker 置为空闲
+            // 如果有等待的任务就拿出来执行
+            const task = waitingTask.shift()
+            if (task) {
+                console.log('do previous task', task)
+                doHashWithWorker(task.req.pieceIndex, task.req.pieceData, task.callback, task.resolve)
+            }
+        }
+    } else { // 等待有可用的 worker
+        console.log('waiting a worker', req)
+        waitingTask.push({ req, callback, resolve })
+    }
+}
+
+export { genFilesSha1 }
---
+++ src/util/worker.ts
@@ -0,0 +1,26 @@
+interface Req {
+    workerIndex: number
+    pieceIndex: number
+    pieceData: Uint8Array
+}
+
+interface Resp {
+    workerIndex: number
+    pieceIndex: number
+    pieceLengh: number
+    pieceHash: Uint8Array
+}
+
+self.addEventListener('message', (e) => {
+    const req = e.data as Req
+    crypto.subtle.digest('SHA-1', req.pieceData).then(result => {
+        self.postMessage({
+            workerIndex: req.workerIndex,
+            pieceIndex: req.pieceIndex,
+            pieceLengh: req.pieceData.byteLength,
+            pieceHash: new Uint8Array(result),
+        } as Resp)
+    })
+});
+
+export type { Req, Resp }
